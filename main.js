d9b654951eb9de8d6a38ca5d38c63dd4a31d5033
SChxmb <137280706+SChxmb@users.noreply.github.com>
2023-09-06 19:28:38 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,152 @@
-function sizeCheck() {
-    if (window.innerWidth >= window.innerHeight && document.getElementById("platformcss").href != "") {
-        document.getElementById("platformcss").href = "";
-        try{sbar.classList.remove("showsbar")} catch {}
-    }
-    if (window.innerWidth < window.innerHeight && document.getElementById("platformcss").href != "Layout/mobile.css") {
-        document.getElementById("platformcss").href = "Layout/mobile.css";
-    }
-    refreshAside();
+// page functions
+
+
+function topBtn(i) {
+    rmvAllClass("seltab")
+    document.getElementById("topbar").rows[0].cells[i].classList.add("seltab");
+    try {document.getElementById(`pg${i.toString()}`).classList.add("seltab")} catch {}
+}
+function toolBTN(i) { 
+    rmvAllClass("seltool")
+    document.getElementById(`tBtn${i}`).classList.add("seltool");
+    document.getElementById(`t${i}_div`).classList.add("seltool");
 }
 function colorCheck() {
     document.getElementById("colorcss").href = "Colour/light.css";
 }
-function openAside() {
-    let sbar = document.getElementsByTagName("aside")[0];
-    let sbarsel = document.getElementById("sidebarsel");
-    if (sbar.classList.length == 0) {
-        sbar.classList.add("showsbar");
-        sbarsel.classList.add("showsbar");
-    } else {
-        sbar.classList.remove("showsbar")
-        sbarsel.classList.remove("showsbar");
-    }
-    refreshAside()
+
+
+// general tools
+
+
+function rmvAllClass(str) {
+    let sel = document.getElementsByClassName(str)
+    for (let x=sel.length-1;x >= 0; x--) {
+        sel[x].classList.remove(str);}
 }
-function tabBTN(selpageid) {
-    for (let currSel of document.getElementsByClassName("selected")) {
-        currSel.classList.remove("selected");
+async function readCSV(loc){
+    async function a(loc) {
+        let response = await fetch(loc);
+        let data = await response.text();
+        return data
     }
-    try {document.getElementsByClassName("selected")[0].classList.remove("selected");}
-    catch {}
-    document.getElementById('nav' + selpageid).classList.add("selected");
-    try {document.getElementById('main' + selpageid).classList.add("selected");} catch {}
-    refreshAside();
+    var csvRows = (await a(loc)).split('\n')
+    var output = []
+    for (let csvRow of csvRows) {output.push(csvRow.split(','))}
+    return output
+}
+async function test() {
+    t0_shrink()
+    document.getElementById("topbar").rows[0].cells[0].innerHTML = Math.floor(Math.random() * 100);
+}
+function ret (x) {
+    document.getElementById("dev").innerHTML += x.toString() + "; "
+    document.getElementById("nav5").innerHTML == x
 }
 
-function refreshAside() {
-    document.getElementsByTagName("aside")[0].style.height = "0px"
-    if (document.body.scrollHeight > window.innerHeight) { 
-        document.getElementsByTagName("aside")[0].style.height = (document.body.scrollHeight - document.getElementById("nullDiv").clientHeight).toString() + "px";
-    } else {
-        document.getElementsByTagName("aside")[0].style.height = (window.innerHeight - document.getElementById("nullDiv").clientHeight).toString() + "px";
+
+// Tool 0, Timetable
+
+
+async function t0_init() {
+    while (document.getElementById('t0_table').rows.length > 0) {document.getElementById('t0_table').deleteRow(0);}
+    while (document.getElementById('t0_name').options.length > 0) {document.getElementById('t0_name').remove(0);}
+    for (let i=0; i <= 4; i++) {rmvAllClass("rCol"+i)}
+    const minSep = 15
+    var table = document.getElementById("t0_table")
+    table.style.width = "20%"
+    table.insertRow(0).insertCell(0).innerHTML = minSep
+    for (let i = 0; i <= (1440 / minSep); i++) {
+        let minPast = minSep * (i + 1)
+        if (i == 0) {
+            var from = "0:00" 
+            var to = `${(Math.floor(minSep/60)).toString()}:${(minSep%60).toString().padStart(2, "0")}`
+        } else {
+            var from = to
+            to = `${(Math.floor(minPast/60)).toString()}:${(minPast%60).toString().padStart(2, "0")}`
+        }
+        table.insertRow(-1).insertCell(0).outerHTML = `<td onclick="t0_highRow(${i+1})">${from} - ${to}</td>`
+    }
+    var CSV = await readCSV("CSV/sch.csv")
+    let nameSel = document.getElementById("t0_name").options
+    for (let i = 0; i < CSV.length / 10; i++) {
+        nameSel[nameSel.length] = new Option(CSV[i * 10][0], i * 10)
     }
 }
+async function t0_add() {
+    var CSV = await readCSV("CSV/sch.csv")
+    var rows = document.getElementById("t0_table").rows
+    var spacer = parseInt(rows[0].cells[0].innerHTML)
+    var dayPlan = CSV[parseInt(document.getElementById("t0_name").value) + parseInt(document.getElementById("t0_day").value) + 2]
+    var cellPos = rows[0].cells.length
+    document.getElementById("t0_table").style.width = (parseInt(document.getElementById("t0_table").style.width.replace("%","")) + 20).toString() + "%"
+    for (let i=0; i < rows.length-1; i++) {rows[i].insertCell(cellPos).outerHTML = `<td onclick="t0_highRow(${i})"></td>`}
+    rows[0].cells[cellPos].innerHTML = document.getElementById("t0_name").options[document.getElementById("t0_name").selectedIndex].text
+    for (let i = 0; i < (dayPlan.length - 1) / 3; i++) {
+        let subject = dayPlan[i*3+1]
+        let start = parseInt(dayPlan[i*3+2].split(":")[0])*60 + parseInt(dayPlan[i*3+2].split(":")[1])
+        let end = parseInt(dayPlan[i*3+3].split(":")[0])*60 + parseInt(dayPlan[i*3+3].split(":")[1])
+        let sIndx = Math.floor(start/spacer) + 1
+        let rCol = "rCol" + Math.floor(Math.random() * 10)%5
+        for (let j = 1; j <= rows.length; j++) {
+            let rtime = (j - 1) * spacer
+            if (j == sIndx) {rows[j].cells[cellPos].innerHTML = subject ; rows[j].cells[cellPos].classList.add(rCol) }
+            else if (j - 1 == sIndx && rtime < end && (rows[j].cells[cellPos].innerHTML == "" || rows[j].cells[cellPos].innerHTML == "^")) {rows[j].cells[cellPos].innerHTML = dayPlan[i*3+2]  + " To " + dayPlan[i*3+3]; rows[j].cells[cellPos].classList.add(rCol)}
+            else if (j - 1 == sIndx) {rows[j-1].cells[cellPos].innerHTML += ": " + dayPlan[i*3+2] + " To " + dayPlan[i*3+3]}
+            else if (((start < rtime) && (rtime < end))&&(rows[j].cells[cellPos].innerHTML == "")) {rows[j].cells[cellPos].innerHTML = "^"; rows[j].cells[cellPos].classList.add(rCol)}
+        }
+    }
+    t0_shrink()
+}
+function t0_shrink() {
+    var rows = document.getElementById("t0_table").rows
+    var start = 4096
+    var end = 0
+    rmvAllClass("t0_hide")
+    for (let i = 1; i < rows.length; i++) {
+        for (let j = 1; j < rows[i].cells.length; j++) {
+            if (!(rows[i].cells[j].innerHTML == "") && start > i) {start = i}
+            if (!(rows[i].cells[j].innerHTML == "") && end < i) {end = i}
+        }
+    }
+    for (let i = 0; i < rows.length; i++) {
+        if ((i+1 < start) || (i-2 > end)) {rows[i].classList.add("t0_hide")}
+        else if (i-2 == end) {rows[i].cells[0].innerHTML = "..."}
+    }
+    rows[0].classList.remove("t0_hide")
+}
+function t0_highRow(i) {
+    var rows = document.getElementById("t0_table").rows
+    if(rows[i].classList.contains("t0_selTime")) {rows[i].classList.remove("t0_selTime")}
+    else {rows[i].classList.add("t0_selTime")}
+}
 
-function test() {
-    document.getElementById("nav1").innerHTML = Math.floor(Math.random() * 100);
+// Defunct
+
+
+async function popAside(lstNo) {
+    var table = document.getElementById("sidebar")
+    var x = table.rows.length
+    for (let i = 1; i < x; i++) {
+        table.deleteRow(-1)
+    }
+    var sch = await readCSV('/CSV/sch.csv')
+    var day = (new Date()).getDay() + 4
+    /*for (let i=1; i < sch[2].length; i++) {
+        if ((i % 2) == 1) {
+            let date = sch[2][i]
+    }*/
+    if (sch[day].length >= 3) {
+    for (let i=1; i < sch[day].length; i++) {
+        if ((i % 3) == 1) {
+            let row = table.insertRow(-1)
+            let time = sch[day][i]
+            let minutes = parseInt(time.substring(time.indexOf(':') + 1)) + parseInt(sch[1][parseInt(sch[day][i+2])*2])
+            let hours = ((parseInt(time.substring(0, time.indexOf(':'))) + Math.floor(minutes/60))%24).toString()
+            minutes = (minutes % 60).toString()
+            if (minutes == "0") {minutes = "00"}
+            let newtime = hours + ":" + minutes
+            row.insertCell(0).outerHTML = "<th>" + time + " to " + newtime + " <br> " + sch[0][parseInt(sch[day][i+1])*2] + "</th>"
+        }
+    }}
 }
