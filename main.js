fb568c62d32b29336ad45de175dba58a9be2dd4b
SChxmb <login@7x3.dev>
2023-09-23 20:26:24 +0100

    few

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function toolBTN(i) {
 }
 
 function colorCheck() {
-    document.getElementById("colorcss").href = "Colour/light.css";
+    document.getElementById("colorcss").href = "Colour/dark.css";
 }
 
 
@@ -43,12 +43,16 @@ async function readCSV(loc){
     for (let csvRow of csvRows) {output.push(csvRow.split(','))}
     return output
 }
+
 async function test() {
+
     let temp = document.getElementById("colorcss")
     if (temp.href.toString().search("Colour/dark.css") >= 0) {temp.href = "Colour/light.css"}
     else {temp.href = "Colour/dark.css"}
     document.getElementById("topbar").rows[0].cells[0].innerHTML = Math.floor(Math.random() * 100);
+
 }
+
 function ret (x) {
     document.getElementById("dev").innerHTML += x.toString() + "; "
     document.getElementById("nav5").innerHTML == x
@@ -57,101 +61,184 @@ function ret (x) {
 
 // Tool 0, Timetable
 
+var CSV = []
+
 async function t0_autofill() {
-    for (let i=0; i < t0_name.length; i++) {
-        document.getElementById("t0_name").selectedIndex = i
-        await t0_add()
-    }
-}
-
-async function t0_reloadSide() {
-    while (document.getElementById('t0_table').rows.length > 0) {document.getElementById('t0_table').deleteRow(0);}
-    while (document.getElementById('t0_name').options.length > 0) {document.getElementById('t0_name').remove(0);}
-
-    for (let i=0; i <= 4; i++) {rmvAllClass("rCol"+i)}
-
-    const minSep = 15
-    var table = document.getElementById("t0_table")
-
-    table.style.width = "20%"
-    table.insertRow(0).insertCell(0).innerHTML = minSep
-    
-    for (let i = 0; i < (1440 / minSep); i++) {
-        
-        let minPast = minSep * (i + 1)
-        
-        if (i == 0) {
-        
-            var from = "0:00" 
-            var to = `${(Math.floor(minSep/60)).toString()}:${(minSep%60).toString().padStart(2, "0")}`
-        
-        } else {
-        
-            var from = to
-            to = `${(Math.floor(minPast/60)).toString()}:${(minPast%60).toString().padStart(2, "0")}`
-        
-        }
-        
-        table.insertRow(-1).insertCell(0).outerHTML = `<td onclick="t0_highRow(${i+1})">${from} - ${to}</td>`
-    
+    let nameBox = document.getElementById('t0_name')
+    for (let i=0; i < nameBox.options.length; i++) {
+        await t0_add(i, document.getElementById("t0_day").selectedIndex, 0)
     }
 }
 
 async function t0_init() {
 
-    await t0_reloadSide()
+    await t0_reload()
     
-    document.getElementById("t0_table").insertRow(-1).insertCell(0).outerHTML = `<td>...</td>`
-
-    var CSV = await readCSV("CSV/sch.csv")
+    CSV = await readCSV("CSV/sch.csv")
+    
     let nameSel = document.getElementById("t0_name").options
     
     for (let i = 0; i < CSV.length / 10; i++) {
-    
         nameSel[nameSel.length] = new Option(CSV[i * 10][0], i * 10)
-    
     }
 
-    t0_autofill()
+//    await t0_autofill()
 
 }
 
-async function t0_add() {
+async function t0_reload() {
 
-    var CSV = await readCSV("CSV/sch.csv")
-    var rows = document.getElementById("t0_table").rows
+    const minSep = 15
+    var table = document.getElementById("t0_table")
 
-    var spacer = parseInt(rows[0].cells[0].innerHTML)
-    var dayPlan = CSV[parseInt(document.getElementById("t0_name").value) + parseInt(document.getElementById("t0_day").value) + 2]
-    var cellPos = rows[0].cells.length
+    while (table.rows.length > 0) {table.deleteRow(0);}
+    while (document.getElementById('t0_name').options.length > 0) {document.getElementById('t0_name').remove(0);}
 
-    document.getElementById("t0_table").style.width = (parseInt(document.getElementById("t0_table").style.width.replace("%","")) + 20).toString() + "%"
+    table.style.width = "20%"
+    table.insertRow(0).insertCell(0).innerHTML = minSep
+    
+    var to = "0:00", from = ''
+
+    for (let i = 0; i < (1440 / minSep); i++) {
+        
+        let minPast = minSep * (i + 1)
+        
+        from = to
+        to = `${(Math.floor(minPast/60)).toString()}:${(minPast%60).toString().padStart(2, "0")}`
+        
+        table.insertRow(-1).insertCell(0).outerHTML = `<td onclick="t0_highRow(${i+1})">${from} - ${to}</td>`
+    
+    }
+
+    table.insertRow(-1).insertCell(0).outerHTML = `<td>...</td>`
+
+}
+
+async function t0_add(nameIndex, dayIndex, mode) {
+
+    let table = document.getElementById("t0_table")
+    let rows = table.rows
+    let selName = document.getElementById("t0_name").options[nameIndex]
+
+    let spacer = parseInt(rows[0].cells[0].innerHTML)
+    let dayPlan = CSV[parseInt(selName.value)+ 2 + dayIndex]
+    let cellPos = rows[0].cells.length
+
+    let tempFSpots = []
+
+    table.style.width = (parseInt(table.style.width.replace("%","")) + 20).toString() + "%"
 
     for (let i=0; i < rows.length-1; i++) {rows[i].insertCell(cellPos).outerHTML = `<td onclick="t0_highRow(${i})"></td>`}
-    
-    rows[0].cells[cellPos].innerHTML = document.getElementById("t0_name").options[document.getElementById("t0_name").selectedIndex].text
-    
+
+    rows[0].cells[cellPos].innerHTML = selName.text
+
     for (let i = 0; i < (dayPlan.length - 1) / 3; i++) {
         
         let subject = dayPlan[i*3+1]
         let start = parseInt(dayPlan[i*3+2].split(":")[0])*60 + parseInt(dayPlan[i*3+2].split(":")[1])
         let end = parseInt(dayPlan[i*3+3].split(":")[0])*60 + parseInt(dayPlan[i*3+3].split(":")[1])
+        
         let sIndx = Math.floor(start/spacer) + 1
         let rCol = "rCol" + Math.floor(Math.random() * 10)%5
+
+        if (mode == 0) {
+            for (let j = 1; j <= rows.length; j++) {
+
+                let rtime = (j - 1) * spacer
+            
+                if (j == sIndx) {rows[j].cells[cellPos].innerHTML = subject ; rows[j].cells[cellPos].classList.add(rCol) }
+                
+                else if (j - 1 == sIndx && rtime < end && (rows[j].cells[cellPos].innerHTML == "" || rows[j].cells[cellPos].innerHTML == "^")) {rows[j].cells[cellPos].innerHTML = dayPlan[i*3+2]  + " To " + dayPlan[i*3+3]; rows[j].cells[cellPos].classList.add(rCol)}
+                else if (j - 1 == sIndx) {rows[j-1].cells[cellPos].innerHTML += ": " + dayPlan[i*3+2] + " To " + dayPlan[i*3+3]}
+                else if (((start < rtime) && (rtime < end))&&(rows[j].cells[cellPos].innerHTML == "")) {rows[j].cells[cellPos].innerHTML = "^"; rows[j].cells[cellPos].classList.add(rCol)}
+                
+            }
+        }
+
+        else if (mode == 1) {
+            
+            for (let j = 1; j <= rows.length; j++) {
+                let rtime = (j - 1) * spacer
+                if (((start <= rtime) && (rtime < end)) && (tempFSpots.indexOf(rtime) < 0)) {tempFSpots.push(rtime)}
+            }
         
-        for (let j = 1; j <= rows.length; j++) {
+        }
+    
+    }
+
+    if (mode == 0) {return}
+    else if (mode == 1) {return tempFSpots}
+
+}
+
+async function t0_getBusyTimes(nameIndex, dayIndex) {
+
+    let table = document.getElementById("t0_table")
+    let rows = table.rows
+    let selName = document.getElementById("t0_name").options[nameIndex]
+
+    let spacer = parseInt(rows[0].cells[0].innerHTML)
+    let dayPlan = CSV[parseInt(selName.value)+ 2 + dayIndex]
+
+    let fSpotDesc = []
+
+    for (let i = 0; i < (dayPlan.length - 1) / 3; i++) {
+        let subject = dayPlan[i*3+1]
+        let start = parseInt(dayPlan[i*3+2].split(":")[0])*60 + parseInt(dayPlan[i*3+2].split(":")[1])
+        let end = parseInt(dayPlan[i*3+3].split(":")[0])*60 + parseInt(dayPlan[i*3+3].split(":")[1])
+        let sIndx = Math.floor(start/spacer) + 1
+        let lenCount = 0
+        
+        for (let j = 0; j < rows.length; j++) {
             
-            let rtime = (j - 1) * spacer
-            
-            if (j == sIndx) {rows[j].cells[cellPos].innerHTML = subject ; rows[j].cells[cellPos].classList.add(rCol) }
-            
-            else if (j - 1 == sIndx && rtime < end && (rows[j].cells[cellPos].innerHTML == "" || rows[j].cells[cellPos].innerHTML == "^")) {rows[j].cells[cellPos].innerHTML = dayPlan[i*3+2]  + " To " + dayPlan[i*3+3]; rows[j].cells[cellPos].classList.add(rCol)}
-            else if (j - 1 == sIndx) {rows[j-1].cells[cellPos].innerHTML += ": " + dayPlan[i*3+2] + " To " + dayPlan[i*3+3]}
-            else if (((start < rtime) && (rtime < end))&&(rows[j].cells[cellPos].innerHTML == "")) {rows[j].cells[cellPos].innerHTML = "^"; rows[j].cells[cellPos].classList.add(rCol)}
+            let rtime = j * spacer
+
+            if ((start <= rtime) && (rtime < end)) {
+                lenCount += 1
+            }
+        
+        }
+
+        fSpotDesc.push([subject, sIndx, lenCount, `${dayPlan[i*3+2]} to ${dayPlan[i*3+3]}`])
+    
+    }
+
+    return fSpotDesc
+
+}
+
+async function t0_addCompare(nameIndex, dayIndex) {
+
+    let table = document.getElementById("t0_table")
+    let rows = table.rows
+    let selName = document.getElementById("t0_name").options[nameIndex]
+    let cellPos = rows[0].cells.length
+
+    table.style.width = (parseInt(table.style.width.replace("%","")) + 20).toString() + "%"
+    for (let i=0; i < rows.length-1; i++) {rows[i].insertCell(cellPos).outerHTML = `<td onclick="t0_highRow(${i})"></td>`}
+    rows[0].cells[cellPos].innerHTML = selName.text
+
+    let tDesc = await t0_getBusyTimes(nameIndex, dayIndex)
+    
+    for (let i=0; i < tDesc.length; i++) {
+        
+        let subject = tDesc[i][0]
+        let sIndx = tDesc[i][1]
+        let lenCount = tDesc[i][2]
+        let times = tDesc[i][3]
+
+        for (let j=sIndx; j < sIndx + lenCount; j++) {
+            if (j == sIndx) {rows[j].cells[cellPos].innerHTML = subject && times}
+            else if (rows[j].cells[cellPos].innerHTML == "") {
+                rows[j].cells[cellPos].innerHTML = "^"
+            }
         }
     }
 
-    t0_shrink()
+}
+
+async function t0_ttblOverlay_add(name) {
+    //for (let day = 0; day <= 6, day++) {}
 }
 
 function t0_shrink() {
